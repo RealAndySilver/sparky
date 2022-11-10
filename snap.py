0e433680f7577e15c15641001b9a3114137b85e5
Andres Abril <andres@apriorins.com>
2022-11-10 06:52:22 +0000

    created udf and added new column to df with snapped time

--- snap.py
+++ snap.py
@@ -1,44 +1,53 @@
 from datetime import datetime
 import math
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
 import findspark
 findspark.init()
 
 spark = SparkSession.builder.master("local[*]").getOrCreate()
-spark.conf.set("spark.sql.repl.eagerEval.enabled", True)  # type: ignore
+spark.conf.set("spark.sql.repl.eagerEval.enabled", "true")  
 
-
-def get_time(date_text):
-    """Get time from date text"""
+def get_timestamp(date_text):
+    """Get timestamp from date text"""
     try:
-        date_text = datetime.strptime(date_text, '%Y/%m/%d %H:%M:%S+00')      
+        date_text = datetime.strptime(date_text, '%Y/%m/%d %H:%M:%S+00')
     except ValueError:
-        date_text = datetime.strptime("1900/01/01 01:01:01+00", '%Y/%m/%d %H:%M:%S+00')
-    
-    return date_text
+        date_text = datetime.strptime(
+            "1900/01/01 01:01:01+00", '%Y/%m/%d %H:%M:%S+00')
 
-def snap_time(timestamp, resolution=1):
+    return date_text.timestamp()
+
+def snap_time_to_resolution(timestamp, resolution=1):
     """Snap time to resolution"""
     if resolution <= 0:
         resolution = 1
-    resolution_ms = resolution * 60 * 1000
-    res = datetime.fromtimestamp(math.floor(timestamp / resolution_ms) * resolution_ms)
-    print(res)
-    return res
+    resolution_ms = resolution * 60
+    snapped_time = datetime.fromtimestamp(
+        math.floor(timestamp / resolution_ms) * resolution_ms)
+
+    return snapped_time
+
+def snap_row(date, resolution=5):
+    """Snap row to resolution"""
+    print('LE DEIT', date)
+    timestamp = get_timestamp(date)
+    snapped_time = snap_time_to_resolution(timestamp, resolution)
+    return snapped_time
+
+snap_udf = udf(snap_row, 'timestamp')
 
 data_df = spark.read.csv('data/underdrain.csv',
                          header=True,
                          inferSchema=True)  # type: ignore
-data_df.printSchema()
+
 # data_df.na.drop(subset=['DCH_INSTALL_DATE']).show(truncate=False)
-filtered_df = data_df.filter(data_df.DCH_INSTALL_DATE != 'None')
+data_df.printSchema()
+filtered_df = data_df.filter(data_df.DCH_INSTALL_DATE.isNotNull())
 
-# data_df.toPandas()
+# filtered_df.foreach(lambda row: snap_row(row[18], 1))  # type: ignore
 
-filtered_df.foreach(
-    lambda x:
-        snap_time(
-            get_time(x[18]).timestamp(),
-            5  # type: ignore
-        )
-    )
+# filtered_df.withColumn('SNAPPED_TIME', snap_row(filtered_df.DCH_INSTALL_DATE, 1)).show(5)    # type: ignore
+filtered_df.withColumn('SNAPPED_TIME', snap_udf(filtered_df.DCH_INSTALL_DATE)).write.csv("data/csvs", header=True)
+# filtered_df.show(10)
+# filtered_df.write.csv("data/csvs", header=True)
